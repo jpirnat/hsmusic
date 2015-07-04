c93d824fb57a3333bf258d0ca7424bb614a8f4b3
Jesse Pirnat <jessepirnat@gmail.com>
2015-07-04 18:26:07 -0400

    Indents for prettiness

--- index.php
+++ index.php
@@ -1,54 +1,54 @@
 <?php
-	require 'vendor/autoload.php';
+require 'vendor/autoload.php';
 
-	$dotenv = new Dotenv\Dotenv(__DIR__);
-	$dotenv->load();
-	
-	$dbhost = getenv('DB_HOST');
-	$dbport = getenv('DB_PORT');
-	$dbname = getenv('DB_NAME');
-	$dbuser = getenv('DB_USER');
-	$dbpass = getenv('DB_PASS');
-	
-	try
-	{
-		$db = new PDO("mysql:host={$dbhost};port={$dbport};dbname={$dbname}", $dbuser, $dbpass);
-	}
-	catch (PDOException $e)
-	{
-		echo 'Connection failed: ' . $e->getMessage();
-	}
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
-	$stmt = $db->query('SELECT song_id, album, track_number, song_title, song_url, song_length FROM songs');
+$dbhost = getenv('DB_HOST');
+$dbport = getenv('DB_PORT');
+$dbname = getenv('DB_NAME');
+$dbuser = getenv('DB_USER');
+$dbpass = getenv('DB_PASS');
 
-	$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+try
+{
+	$db = new PDO("mysql:host={$dbhost};port={$dbport};dbname={$dbname}", $dbuser, $dbpass);
+}
+catch (PDOException $e)
+{
+	echo 'Connection failed: ' . $e->getMessage();
+}
 
-	function getSongArtists($db, $song_id)
-	{
-		$stmt = $db->prepare('SELECT artcred_text FROM artist_credits WHERE song_id=:song_id');
-		$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
-		$stmt->execute();
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		return $rows;
-	}
+$stmt = $db->query('SELECT song_id, album, track_number, song_title, song_url, song_length FROM songs');
 
-	function getSongRemixes($db, $song_id)
-	{
-		$stmt = $db->prepare('SELECT song_title, song_url FROM songs WHERE song_id IN (SELECT beingremixed_id FROM remixes WHERE song_id=:song_id)');
-		$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
-		$stmt->execute();
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		return $rows;
-	}
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-	function getSongCommentaries($db, $song_id)
-	{
-		$stmt = $db->prepare('SELECT commentary_url FROM Commentaries WHERE song_id=:song_id');
-		$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
-		$stmt->execute();
-		$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		return $rows;
-	}
+function getSongArtists($db, $song_id)
+{
+	$stmt = $db->prepare('SELECT artcred_text FROM artist_credits WHERE song_id=:song_id');
+	$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $rows;
+}
+
+function getSongRemixes($db, $song_id)
+{
+	$stmt = $db->prepare('SELECT song_title, song_url FROM songs WHERE song_id IN (SELECT beingremixed_id FROM remixes WHERE song_id=:song_id)');
+	$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $rows;
+}
+
+function getSongCommentaries($db, $song_id)
+{
+	$stmt = $db->prepare('SELECT commentary_url FROM Commentaries WHERE song_id=:song_id');
+	$stmt->bindValue(':song_id', $song_id, PDO::PARAM_INT);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $rows;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -72,44 +72,44 @@
 				</tr>
 			</thead>
 			<tbody>
-<?php foreach ($results as $row): ?>
-<tr>
-	<td><?php echo $row['album']; ?></td>
-	<td><?php echo $row['track_number']; ?></td>
-	<td><a href="<?php echo $row['song_url']; ?>"><?php echo $row['song_title']; ?></a></td>
-	<td><?php echo $row['song_length']; ?></td>
+				<?php foreach ($results as $row): ?>
+				<tr>
+					<td><?php echo $row['album']; ?></td>
+					<td><?php echo $row['track_number']; ?></td>
+					<td><a href="<?php echo $row['song_url']; ?>"><?php echo $row['song_title']; ?></a></td>
+					<td><?php echo $row['song_length']; ?></td>
 
-	<td>
-<?php // artists
-$artists = getSongArtists($db, $row['song_id']);
-foreach ($artists as $artist)
-{
-	echo $artist['artcred_text']. '<br>';
-}
-?>
-	</td>
-	
-	<td>
-<?php // remixes
-$remixes = getSongRemixes($db, $row['song_id']);
-foreach ($remixes as $remix)
-{
-	echo "<a href='{$remix['song_url']}'>{$remix['song_title']}</a><br>";
-}
-?>
-	</td>
-	
-	<td>
-<?php // commentary
-$commentaries = getSongCommentaries($db, $row['song_id']);
-foreach ($commentaries as $commentary)
-{
-	echo $commentary['commentary_url'] . '<br>';
-}
-?>
-	</td>
-</tr>
-<?php endforeach; ?>
+					<td>
+				<?php // artists
+				$artists = getSongArtists($db, $row['song_id']);
+				foreach ($artists as $artist)
+				{
+					echo $artist['artcred_text']. '<br>';
+				}
+				?>
+					</td>
+					
+					<td>
+				<?php // remixes
+				$remixes = getSongRemixes($db, $row['song_id']);
+				foreach ($remixes as $remix)
+				{
+					echo "<a href='{$remix['song_url']}'>{$remix['song_title']}</a><br>";
+				}
+				?>
+					</td>
+					
+					<td>
+				<?php // commentary
+				$commentaries = getSongCommentaries($db, $row['song_id']);
+				foreach ($commentaries as $commentary)
+				{
+					echo $commentary['commentary_url'] . '<br>';
+				}
+				?>
+					</td>
+				</tr>
+				<?php endforeach; ?>
 			</tbody>
 		</table>
 
